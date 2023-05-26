8b61d6e93fbaf7b47d15bd17f672d579fc12b9d6
rodriguesfas <franciscosouzaacer@gmail.com>
2023-05-26 11:08:37 -0300

    updated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,6 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Francisco Rodrigues",
-  themes: ['@docusaurus/theme-search-algolia'],
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
